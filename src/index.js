cb4b4ad4acd59f1e3ecc078c8043c0c2ce57ac2a
Elise Erickson <elisece717@gmail.com>
2023-11-28 16:08:23 -0500

    adds ability to push form inputs to the To-Do List

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  //find form button
+  let taskForm = document.getElementById("create-task-form");
+   // add event to form
+   taskForm.addEventListener("submit", (e) => handleTaskSubmit(e));
+ 
+  //event prevents default
+  //event creates new p element
+  // p element comes with an event listener to delete itself
+  //add p
 });
+
+handleTaskSubmit = (e) => {
+  //prevent default
+  e.preventDefault();
+  //create p element
+  const toDoEntry = document.createElement("li");
+
+  const toDoText = document.createElement("p");
+  toDoText.textContent = e.target.querySelector("#new-task-description").value;
+
+  const toDoButton = document.createElement("button");
+  toDoButton.textContent = "X";
+  //add listener to p element
+  toDoButton.addEventListener("click", (event) => {
+    event.target.remove();
+  });
+
+  //push p element
+  toDoEntry.appendChild(toDoText);
+  toDoEntry.appendChild(toDoButton);
+  let list = document.getElementsByTagName("ul")[0];
+  list.appendChild(toDoEntry);
+  //reset form
+  e.target.reset();
+}
